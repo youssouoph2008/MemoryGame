320b6c0ffb0a7728173daf30a09cc6d0e8a54b67
Youssouph <diakhite2008@gmail.com>
2026-04-12 12:20:34 +0200

    Intento de que funcione

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react";
 import App from "./App";
-import "index.css";
+import "/src/index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
